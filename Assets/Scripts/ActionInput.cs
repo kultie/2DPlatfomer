13c9b84303a8e6f85807813b4f8b7346ad30b5a5
Kultie <putin1721996@gmail.com>
2023-10-28 22:01:03 +0700

    Add MMFeel and animancer

--- Assets/Scripts/ActionInput.cs
+++ Assets/Scripts/ActionInput.cs
@@ -8,8 +8,10 @@ namespace Kultie.Platformer2DSystem
     public class ActionInput : MonoBehaviour
     {
         private PlayerController _owner;
-        [SerializeField] private EntityAction airAttack;
         [SerializeField] private EntityAction groundAttack;
+        [SerializeField] private EntityAction skill1;
+        [SerializeField] private EntityAction skill2;
+        [SerializeField] private EntityAction airAttack;
         private void Awake()
         {
             _owner = GetComponent<PlayerController>();
@@ -33,6 +35,14 @@ namespace Kultie.Platformer2DSystem
             if (Input.GetKeyDown(KeyCode.Z))
             {
                 _owner.StartAction(groundAttack.Process(_owner));
+            }
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                _owner.StartAction(skill1.Process(_owner));
+            }
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                _owner.StartAction(skill2.Process(_owner));
             }
         }
 
--- Assets/Scripts/GroundAttack.cs
+++ Assets/Scripts/GroundAttack.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Animancer;
 using UnityEngine;
 using UnityEngine.Animations;
 using UnityEngine.Playables;
@@ -9,7 +10,7 @@ namespace Kultie.Platformer2DSystem
     public class GroundAttack : EntityAction
     {
         [SerializeField] private AnimationClip actionClip;
-        [SerializeField] private Animator animator;
+        [SerializeField] private AnimancerComponent animator;
 
         private PlayableGraph _playableGraph;
 
@@ -17,19 +18,42 @@ namespace Kultie.Platformer2DSystem
         {
             // AnimationPlayableUtilities.PlayClip(animator, actionClip, out _playableGraph);
             // _playableGraph.SetTimeUpdateMode(DirectorUpdateMode.Manual);
-            _playableGraph = PlayableGraph.Create();
-            var animatorOutput = AnimationPlayableOutput.Create(_playableGraph, "Action", animator);
-            var playableClip = AnimationClipPlayable.Create(_playableGraph, actionClip);
-            animatorOutput.SetSourcePlayable(playableClip);
-            _playableGraph.SetTimeUpdateMode(DirectorUpdateMode.GameTime);
-            _playableGraph.Play();
-            float duration = actionClip.length;
-            // animator.speed = 0.5f;
-            while (duration > 0)
+            // _playableGraph = PlayableGraph.Create();
+            // var animatorOutput = AnimationPlayableOutput.Create(_playableGraph, "Action", animator);
+            // var playableClip = AnimationClipPlayable.Create(_playableGraph, actionClip);
+            // animatorOutput.SetSourcePlayable(playableClip);
+            // _playableGraph.SetTimeUpdateMode(DirectorUpdateMode.GameTime);
+            // _playableGraph.Play();
+            // float duration = actionClip.length;
+            // // animator.speed = 0.5f;
+            // while (duration > 0)
+            // {
+            //     duration -= Time.deltaTime;
+            //     yield return null;
+            // }
+
+            var state = animator.Play(actionClip);
+            // state.Time = 0;
+            // float time = 0;
+            // yield return state;
+            // state.Speed = Random.Range(0.02f, 0.5f);
+            while (state.NormalizedTime < 1)
             {
-                duration -= Time.deltaTime;
+                // state.Speed = 0.5f;
                 yield return null;
             }
+
+            state.Speed = 1;
+            // animator.Playable.PauseGraph();
+            // animator.Stop();
+            // while (state.NormalizedTime < 1)
+            // {
+            //     state.Time += Time.deltaTime;
+            //     animator.Evaluate(Time.deltaTime);
+            //     yield return null;
+            // }
+
+            animator.Stop();
 
             // yield return new WaitForSeconds(actionClip.length);
 
@@ -38,8 +62,9 @@ namespace Kultie.Platformer2DSystem
             //     // _playableGraph.Evaluate(Time.deltaTime);
             //     yield return null;
             // }
+            Debug.Log("Finish");
 
-            _playableGraph.Destroy();
+            // _playableGraph.Destroy();
         }
     }
 }
